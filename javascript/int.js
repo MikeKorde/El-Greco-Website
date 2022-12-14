0f62bba2d1cbabbe4f88698cc38e2ce831a5deea
MikeKorde <mix.kordellas@gmail.com>
2022-12-14 19:44:04 +0200

    w

--- javascript/int.js
+++ javascript/int.js
@@ -274,7 +274,6 @@ function topFunction() {
     document.documentElement.style.scrollBehavior = "smooth";
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-    
 }
 var  flag=0;
 $(document).ready(function(){
