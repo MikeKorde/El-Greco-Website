1250ac83f5f7c8dfdaf9c2cf1ea7f5e7527763e9
MikeKorde <mix.kordellas@gmail.com>
2024-05-10 10:41:28 +0200

    e

--- javascript/intgr.js
+++ javascript/intgr.js
@@ -100,6 +100,10 @@ function buildPor(){
                 <div><h2>3,90€</h2><br></div>
             </div>
             <div class="meat">
+                <div><h2>Πανσέτα</h2></div>
+                <div><h2>3,90€</h2><br></div>
+            </div>
+            <div class="meat">
                 <div><h2>Για χορτοφαγική επιλογή ρωτήστε το προσωπικό</h2><br><br></div>
             </div>
 
